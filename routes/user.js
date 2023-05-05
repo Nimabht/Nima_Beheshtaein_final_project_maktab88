ade95f6d8c1c57e9e37c952a245b9d9adcfb7ba9
nimabht <nimabeheshtaein99@gmail.com>
2023-05-06 01:12:47 +0330

    [feat] userAPI router added.

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+import controllers from "../controllers/user.js";
+import express from "express";
+import asyncMiddleware from "../middlewares/async.js";
+import getUser from "../middlewares/user/getUser.js";
+
+const router = express.Router();
+
+router.param("userId", getUser);
+
+router.get("/:userId", asyncMiddleware(controllers.getUserById));
+// router.post("/", asyncMiddleware(controllers.createUser));
+router.put("/:userId", asyncMiddleware(controllers.updateUser));
+// router.delete("/:userId", asyncMiddleware(controllers.deleteUser));
+export default router;

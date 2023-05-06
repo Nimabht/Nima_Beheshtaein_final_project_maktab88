52887eb8f63d64641f6930fee143f0c0abe03c09
nimabht <nimabeheshtaein99@gmail.com>
2023-05-07 00:43:31 +0330

    [refactor] auth matters added to user routes.

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,27 @@ import controllers from "../controllers/user.js";
 import express from "express";
 import asyncMiddleware from "../middlewares/async.js";
 import getUser from "../middlewares/user/getUser.js";
+import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
+import hasAccess from "../middlewares/auth/hasAccess.js";
 
 const router = express.Router();
 
 router.param("userId", getUser);
 
-router.get("/:userId", asyncMiddleware(controllers.getUserById));
+router.get(
+  "/:userId",
+  [checkSessionValidity, hasAccess],
+  asyncMiddleware(controllers.getUserById)
+);
 // router.post("/", asyncMiddleware(controllers.createUser));
-router.put("/:userId", asyncMiddleware(controllers.updateUser));
-router.delete("/:userId", asyncMiddleware(controllers.deleteUser));
+router.put(
+  "/:userId",
+  [checkSessionValidity, hasAccess],
+  asyncMiddleware(controllers.updateUser)
+);
+router.delete(
+  "/:userId",
+  [checkSessionValidity, hasAccess],
+  asyncMiddleware(controllers.deleteUser)
+);
 export default router;

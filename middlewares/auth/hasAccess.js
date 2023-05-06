68166713cc6bead52972e1ea04ae4e27956cb4a5
nimabht <nimabeheshtaein99@gmail.com>
2023-05-07 00:42:33 +0330

    [feat] has access added.

---
+++ middlewares/auth/hasAccess.js
@@ -0,0 +1,16 @@
+import AppError from "../../utils/AppError.js";
+
+export default (req, res, next) => {
+  const session = req.session;
+  //If user is admin, can access to the next middleware
+  if (session.user.role === "admin") {
+    return next();
+  }
+  //If user is blogger, can access to the next middleware if it's requested its own data
+  if (res.locals.user.id === session.user.id) {
+    return next();
+  }
+
+  const ex = new AppError("Unauthorized", "fail", 401);
+  return next(ex);
+};

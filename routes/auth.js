2346384d8e6c98818f6e1b50e92bec65c8cdb536
nimabht <nimabeheshtaein99@gmail.com>
2023-05-07 00:48:01 +0330

    [refactor] auth matter added to logout router

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
 import asyncMiddleware from "../middlewares/async.js";
 import controllers from "../controllers/auth.js";
+import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
 const router = express.Router();
 
 router.post("/signup", asyncMiddleware(controllers.signupUser));
 router.post("/login", asyncMiddleware(controllers.loginUser));
-
+router.get(
+  "/logout",
+  checkSessionValidity,
+  asyncMiddleware(controllers.logoutUser)
+);
 export default router;

649e3ef36dc46d3c254ed00c92f190fd556b2f36
nimabht <nimabeheshtaein99@gmail.com>
2023-05-06 17:07:23 +0330

    [refactor] session added to app.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import db from "./db/connection.js";
 import express from "express";
 // import cookieParser from "cookie-parser";
-// import { default as connectMongoDBSession } from "connect-mongodb-session";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import apiRouter from "./routes/api.js";
-// import user from "./routes/user.js";
 // import index from "./routes/index.js";
-// import session from "express-session";
-// import auth from "./routes/auth.js";
-// const MongoDBStore = connectMongoDBSession(session);
+import session from "express-session";
 
 const app = express();
 
@@ -16,6 +12,14 @@ app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(
+  session({
+    secret: "test-secret",
+    resave: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+  })
+);
 
 app.get("/isConnect", (req, res, next) => {
   res.send("OK");

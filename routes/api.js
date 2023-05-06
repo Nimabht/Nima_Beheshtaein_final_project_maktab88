2e1b73c14ec272ef18ecb933558732c6f2a84980
nimabht <nimabeheshtaein99@gmail.com>
2023-05-06 16:46:58 +0330

    [refactor] auth api added

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 import express from "express";
 import userRouter from "./user.js";
-
+import authRouter from "./auth.js";
 const apiRouter = express.Router();
 
 apiRouter.use("/user", userRouter);
+apiRouter.use("/auth", authRouter);
 
 export default apiRouter;

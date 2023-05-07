37172d812e5cf321a653aaebabbefc1862c43966
nimabht <nimabeheshtaein99@gmail.com>
2023-05-07 23:34:08 +0330

    [feat] user profile page added

---
+++ public/scripts/userProfile/logout.js
@@ -0,0 +1,28 @@
+let polipop = new Polipop("polipop", {
+  layout: "popups",
+  insert: "before",
+  pool: 1,
+  life: 2000,
+  progressbar: true,
+});
+$("#logout-btn").on("click", () => {
+  axios
+    .get("/api/auth/logout")
+    .then((response) => {
+      polipop.add({
+        type: "success",
+        title: "Success",
+        content: "Logout successfully!",
+      });
+      setTimeout(() => {
+        window.location.href = `/login`;
+      }, 2000);
+    })
+    .catch((error) => {
+      polipop.add({
+        type: "error",
+        title: "Error",
+        content: "Something went wrong!!!",
+      });
+    });
+});

2c05aa1b7df91f511c5ee873479dfe866e633fd2
nimabht <nimabeheshtaein99@gmail.com>
2023-05-07 23:33:28 +0330

    [refactor] login and dashboard page controller added to viewPage.

--- routes/viewPages.js
+++ routes/viewPages.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-// import { User } from "../models/user.js";
+import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
+import { User } from "../models/user.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
@@ -15,27 +16,27 @@ router.get("/signup", (req, res, next) => {
   res.sendFile(filePath);
 });
 
-// router.get("/login", (req, res, next) => {
-//   if (req.session && req.session.user) {
-//     // if the user is logged in, redirect to the dashboard
-//     return res.redirect("/dashboard");
-//   }
-//   const filePath = join(__dirname, "../views", "login.html");
-//   res.sendFile(filePath);
-// });
+router.get("/login", (req, res, next) => {
+  if (req.session && req.session.user) {
+    // if the user is logged in, redirect to the dashboard
+    return res.redirect("/dashboard");
+  }
+  const filePath = join(__dirname, "../views", "login.html");
+  res.sendFile(filePath);
+});
 
-// router.get("/dashboard", checkSessionId, async (req, res, next) => {
-//   const { firstname, lastname, username, gender, role, _id } =
-//     await User.findById(req.session.user._id);
-//   res.render("dashboard", {
-//     firstname,
-//     lastname,
-//     username,
-//     gender,
-//     role,
-//     _id,
-//   });
-// });
+router.get("/dashboard", checkSessionValidity, async (req, res, next) => {
+  const { firstname, lastname, username, phoneNumber, role, avatarFileName } =
+    await User.findById(req.session.user._id);
+  res.render("userProfile", {
+    firstname,
+    lastname,
+    username,
+    phoneNumber,
+    role,
+    avatarFileName,
+  });
+});
 
 // router.get("/resetpassword", checkSessionId, async (req, res, next) => {
 //   const { _id } = await User.findById(req.session.user._id);

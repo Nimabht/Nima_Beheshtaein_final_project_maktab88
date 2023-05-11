d403f17134d028756d2719fed0e7ec29c8a82956
nimabht <nimabeheshtaein99@gmail.com>
2023-05-11 18:19:12 +0330

    [refactor] multer conf updated to diskStorage type.

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -1,40 +1,42 @@
 import multer from "multer";
 import AppError from "./AppError.js";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/avatars");
-  },
-  filename: function (req, file, cb) {
-    if (
-      file.originalname === "female-anonymous.png" ||
-      file.originalname === "male-anonymous.png" ||
-      file.originalname === ""
-    ) {
-      const ex = AppError.badRequest("Bad file name!");
-      cb(ex, null);
-    } else {
-      cb(null, Date.now() + "-" + file.originalname);
-    }
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/avatars");
+//   },
+//   filename: function (req, file, cb) {
+//     if (
+//       file.originalname === "female-anonymous.png" ||
+//       file.originalname === "male-anonymous.png" ||
+//       file.originalname === ""
+//     ) {
+//       const ex = AppError.badRequest("Bad file name!");
+//       cb(ex, null);
+//     } else {
+//       cb(null, Date.now() + "-" + file.originalname);
+//     }
+//   },
+// });
+
+const storage = multer.memoryStorage();
+
+const muleterFilter = (req, file, cb) => {
+  if (
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    const ex = AppError.badRequest("Only .png, .jpg and .jpeg format allowed!");
+    return cb(ex, false);
+  }
+};
 
 export const upload = multer({
   storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
-    ) {
-      cb(null, true);
-    } else {
-      const ex = AppError.badRequest(
-        "Only .png, .jpg and .jpeg format allowed!"
-      );
-      return cb(ex, false);
-    }
-  },
+  fileFilter: muleterFilter,
   limits: {
     files: 1,
     fileSize: 1 * 1024 * 1024,

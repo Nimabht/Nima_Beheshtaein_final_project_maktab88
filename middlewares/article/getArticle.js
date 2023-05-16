6a96527c0f30568897e25db9d776d6f816ae9942
nimabht <nimabeheshtaein99@gmail.com>
2023-05-16 14:24:26 +0330

    [feat] get article middleware created.

---
+++ middlewares/article/getArticle.js
@@ -0,0 +1,22 @@
+import AppError from "../../utils/AppError.js";
+import isValidObjectId from "../../validators/ObjectId.js";
+import { Article } from "../../models/article.js";
+
+export default async function (req, res, next, articleId) {
+  try {
+    if (!isValidObjectId(articleId)) {
+      const ex = AppError.badRequest("Invalid articleId");
+      return next(ex);
+    }
+    const article = await Article.findById(articleId).select("-__v");
+    if (!article) {
+      const ex = AppError.notFound("Article not found");
+      return next(ex);
+    }
+    res.locals.article = article;
+    next();
+  } catch (error) {
+    const ex = AppError.internal(error.message);
+    return next(ex);
+  }
+}

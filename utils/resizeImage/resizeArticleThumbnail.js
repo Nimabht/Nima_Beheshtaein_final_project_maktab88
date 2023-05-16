4b71e357a8e97d55d9d7e83a50ae1c66fae847c0
nimabht <nimabeheshtaein99@gmail.com>
2023-05-16 14:27:09 +0330

    [feat] resize for thumbnail created.

---
+++ utils/resizeImage/resizeArticleThumbnail.js
@@ -0,0 +1,25 @@
+import sharp from "sharp";
+import { join, dirname, extname } from "node:path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default async (file) => {
+  if (!file) return null;
+
+  const fileExtension = extname(file.originalname);
+  const originalFileNameWithoutExtension = file.originalname.replace(
+    fileExtension,
+    ""
+  );
+
+  const articleThumbnailName = `${Date.now()}-${originalFileNameWithoutExtension}.jpeg`;
+
+  await sharp(file.buffer)
+    .resize(504, 504)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(join(__dirname, `../../public/thumbnails/${articleThumbnailName}`));
+
+  return articleThumbnailName;
+};

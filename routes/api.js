33ca4b7aa5d4a2bf95701fa679e24390f0514c98
nimabht <nimabeheshtaein99@gmail.com>
2023-05-16 11:36:18 +0330

    [refactor] article route added to api route.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 import express from "express";
 import userRouter from "./user.js";
 import authRouter from "./auth.js";
+import articleRouter from "./article.js";
 const apiRouter = express.Router();
 
 apiRouter.use("/user", userRouter);
 apiRouter.use("/auth", authRouter);
-
+apiRouter.use("/article", articleRouter);
 export default apiRouter;

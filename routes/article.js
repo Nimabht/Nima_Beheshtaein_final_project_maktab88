e627b06085a8642533de911130e359c5d67c8f87
nimabht <nimabeheshtaein99@gmail.com>
2023-05-16 14:25:34 +0330

    [feat] article route created.

---
+++ routes/article.js
@@ -0,0 +1,41 @@
+import controllers from "../controllers/article.js";
+import express from "express";
+import asyncMiddleware from "../middlewares/async.js";
+import getArticle from "../middlewares/article/getArticle.js";
+import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
+import hasAccessToArticle from "../middlewares/auth/hasAccessToArticle.js";
+import { uploadThumbnail } from "../utils/multerConfig.js";
+
+const router = express.Router();
+
+router.param("articleId", getArticle);
+
+router.get("", asyncMiddleware(controllers.getAllArticles));
+router.get("/:articleId", asyncMiddleware(controllers.getArticleById));
+router.post(
+  "/",
+  [checkSessionValidity, uploadThumbnail.single("thumbnail")],
+  asyncMiddleware(controllers.createArticle)
+);
+
+// router.put(
+//   "/:articleId",
+//   [checkSessionValidity, hasAccessToArticle],
+//   asyncMiddleware(controllers.updateArticle)
+// );
+router.delete(
+  "/:articleId",
+  [checkSessionValidity, hasAccessToArticle],
+  asyncMiddleware(controllers.deleteArticle)
+);
+router.patch(
+  "/update-thumbnail/:articleId",
+  [
+    checkSessionValidity,
+    hasAccessToArticle,
+    uploadThumbnail.single("thumbnail"),
+  ],
+  asyncMiddleware(controllers.updateArticleThumbnail)
+);
+
+export default router;

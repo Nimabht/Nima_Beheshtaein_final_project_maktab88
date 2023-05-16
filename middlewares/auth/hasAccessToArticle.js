b50cf943e32c97132ebdd5fcfb533fab0d8d4f96
nimabht <nimabeheshtaein99@gmail.com>
2023-05-16 14:25:13 +0330

    [feat] has access (user) to article middleware created.

---
+++ middlewares/auth/hasAccessToArticle.js
@@ -0,0 +1,17 @@
+import AppError from "../../utils/AppError.js";
+
+export default (req, res, next) => {
+  const session = req.session;
+  //If user is admin, can access to the next middleware
+  if (session.user.role === "admin") {
+    return next();
+  }
+  //If user is blogger, can access to the next middleware if it's requested its own data
+  const userIdInArticle = res.locals.article.author.toString();
+  if (userIdInArticle === session.user._id) {
+    return next();
+  }
+
+  const ex = new AppError("Unauthorized", "fail", 401);
+  return next(ex);
+};

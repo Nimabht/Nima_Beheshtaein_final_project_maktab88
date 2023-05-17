1d97d5aba6e76661dffbdd8d469b374eb13de86e
nimabht <nimabeheshtaein99@gmail.com>
2023-05-17 11:08:34 +0330

    [refactor] 1- my-articles route added 2- view count middle ware added to get article by id route.

--- routes/article.js
+++ routes/article.js
@@ -4,6 +4,7 @@ import asyncMiddleware from "../middlewares/async.js";
 import getArticle from "../middlewares/article/getArticle.js";
 import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
 import hasAccessToArticle from "../middlewares/auth/hasAccessToArticle.js";
+import countArticleView from "../middlewares/article/countArticleView.js";
 import { uploadThumbnail } from "../utils/multerConfig.js";
 
 const router = express.Router();
@@ -11,18 +12,28 @@ const router = express.Router();
 router.param("articleId", getArticle);
 
 router.get("", asyncMiddleware(controllers.getAllArticles));
-router.get("/:articleId", asyncMiddleware(controllers.getArticleById));
+router.get(
+  "/my-articles",
+  checkSessionValidity,
+  asyncMiddleware(controllers.getAllUserArticles)
+);
+router.get(
+  "/:articleId",
+  countArticleView,
+  asyncMiddleware(controllers.getArticleById)
+);
 router.post(
   "/",
   [checkSessionValidity, uploadThumbnail.single("thumbnail")],
   asyncMiddleware(controllers.createArticle)
 );
 
-// router.put(
-//   "/:articleId",
-//   [checkSessionValidity, hasAccessToArticle],
-//   asyncMiddleware(controllers.updateArticle)
-// );
+router.put(
+  "/:articleId",
+  [checkSessionValidity, hasAccessToArticle],
+  asyncMiddleware(controllers.updateArticle)
+);
+
 router.delete(
   "/:articleId",
   [checkSessionValidity, hasAccessToArticle],

1e5c6536ef269e2acc647c3f1450d2cae588fa76
nimabht <nimabeheshtaein99@gmail.com>
2023-05-17 23:17:41 +0330

    [refactor] /new-story added to viewPages route

--- routes/viewPages.js
+++ routes/viewPages.js
@@ -51,7 +51,14 @@ router.get(
     });
   }
 );
-
+router.get("/new-story", checkSessionValidity, async (req, res, next) => {
+  // const { firstname, lastname, username, phoneNumber, role, avatarFileName } =
+  //   await User.findById(req.session.user._id);
+  res.render("new-story", {
+    userLoggedIn: req.session.user,
+    inDashboard: false,
+  });
+});
 // router.get("/resetpassword", checkSessionId, async (req, res, next) => {
 //   const { _id } = await User.findById(req.session.user._id);
 //   res.render("resetPassword", {

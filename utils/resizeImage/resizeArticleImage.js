11172c2dadabc8c4fbf8104a689069477dfdb930
nimabht <nimabeheshtaein99@gmail.com>
2023-05-23 01:05:18 +0330

    [feat] resize image article created.

---
+++ utils/resizeImage/resizeArticleImage.js
@@ -0,0 +1,26 @@
+import sharp from "sharp";
+import { join, dirname, extname } from "node:path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default async (file) => {
+  if (!file) return null;
+
+  const fileExtension = extname(file.originalname);
+  const originalFileNameWithoutExtension = file.originalname.replace(
+    fileExtension,
+    ""
+  );
+
+  const articleThumbnailName = `${Date.now()}-${originalFileNameWithoutExtension}.jpeg`;
+
+  await sharp(file.buffer)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(
+      join(__dirname, `../../public/articleImages/${articleThumbnailName}`)
+    );
+
+  return articleThumbnailName;
+};

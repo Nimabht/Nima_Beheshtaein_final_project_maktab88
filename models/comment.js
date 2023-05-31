8f10e2a22cf2957321ac0826db8e8b71ede8f7f7
nimabht <nimabeheshtaein99@gmail.com>
2023-05-31 22:24:13 +0330

    [feat] comment model created.

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    article: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Article",
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);

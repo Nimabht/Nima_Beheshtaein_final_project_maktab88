da4d99fafb6390df724fcf9f081e78bf90155043
nimabht <nimabeheshtaein99@gmail.com>
2023-06-01 01:09:58 +0330

    [refactor] comment route added to api route

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ import express from "express";
 import userRouter from "./user.js";
 import authRouter from "./auth.js";
 import articleRouter from "./article.js";
+import commentRouter from "./comment.js";
 const apiRouter = express.Router();
 
 apiRouter.use("/user", userRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/article", articleRouter);
+apiRouter.use("/comment", commentRouter);
 export default apiRouter;

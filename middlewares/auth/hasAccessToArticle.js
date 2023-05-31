c7a737abe199e9f0f8432fddcf859b13addb4cc6
nimabht <nimabeheshtaein99@gmail.com>
2023-05-31 23:48:40 +0330

    [refactor] comment checking added to hasAccessToArticle

--- middlewares/auth/hasAccessToArticle.js
+++ middlewares/auth/hasAccessToArticle.js
@@ -1,14 +1,22 @@
 import AppError from "../../utils/AppError.js";
 
+//FIXME: if admin pass this middleware (that it passes through) he can edit the article or the comment. i should stop this and only give access to the admin for deleting the article or the comment.
 export default (req, res, next) => {
   const session = req.session;
   //If user is admin, can access to the next middleware
   if (session.user.role === "admin") {
     return next();
   }
-  //If user is blogger, can access to the next middleware if it's requested its own data
-  const userIdInArticle = res.locals.article.author._id.toString();
-  if (userIdInArticle === session.user._id) {
+  //If user is blogger, can access to the next middleware if it's requested its own data (article / comment)
+
+  let userIdInModel = "";
+  if (!!res.locals.article) {
+    userIdInModel = res.locals.article.author._id.toString();
+  }
+  if (!!!!res.locals.comment) {
+    userIdInModel = res.locals.comment.author._id.toString();
+  }
+  if (userIdInModel === session.user._id) {
     return next();
   }
 

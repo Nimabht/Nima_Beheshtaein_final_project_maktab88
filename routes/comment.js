c65618f42d71b54e597a507cab61e53ace2a3815
nimabht <nimabeheshtaein99@gmail.com>
2023-06-01 01:09:40 +0330

    [feat] comment route created.

---
+++ routes/comment.js
@@ -0,0 +1,56 @@
+import controllers from "../controllers/comment.js";
+import express from "express";
+import asyncMiddleware from "../middlewares/async.js";
+// import getComment from "../middlewares/article/getArticle.js";
+import checkSessionValidity from "../middlewares/auth/checkSessionValidity.js";
+import hasAccessToData from "../middlewares/auth/hasAccessToData.js";
+import getComment from "../middlewares/comment/getComment.js";
+
+const router = express.Router();
+
+router.param("commentId", getComment);
+
+// router.get("", asyncMiddleware(controllers.getAllArticles));
+// router.get(
+//   "/my-articles",
+//   checkSessionValidity,
+//   asyncMiddleware(controllers.getAllUserArticles)
+// );
+// router.get(
+//   "/:articleId",
+//   countArticleView,
+//   asyncMiddleware(controllers.getArticleById)
+// );
+router.post(
+  "/",
+  checkSessionValidity,
+  asyncMiddleware(controllers.createComment)
+);
+
+router.put(
+  "/:commentId",
+  [checkSessionValidity, hasAccessToData],
+  asyncMiddleware(controllers.updateComment)
+);
+
+router.delete(
+  "/:commentId",
+  [checkSessionValidity, hasAccessToData],
+  asyncMiddleware(controllers.deleteComment)
+);
+// router.patch(
+//   "/update-thumbnail/:articleId",
+//   [
+//     checkSessionValidity,
+//     hasAccessToArticle,
+//     uploadThumbnail.single("thumbnail"),
+//   ],
+//   asyncMiddleware(controllers.updateArticleThumbnail)
+// );
+
+// router.post(
+//   "/uploadImage",
+//   [checkSessionValidity, uploadImage.single("image")],
+//   asyncMiddleware(controllers.uploadArticleImage)
+// );
+export default router;

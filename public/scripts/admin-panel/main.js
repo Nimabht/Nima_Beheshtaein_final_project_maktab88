c20d6150c78579db14e9838c0b86d5660b4b9c73
nimabht <nimabeheshtaein99@gmail.com>
2023-06-03 20:04:38 +0330

    [feat] admin panel created.

---
+++ public/scripts/admin-panel/main.js
@@ -0,0 +1,8 @@
+$(async () => {
+  let result = await axios.get("/api/user");
+  $("#total-user-out").text(result.data.total);
+  result = await axios.get("/api/article");
+  $("#total-article-out").text(result.data.total);
+  result = await axios.get("/api/comment");
+  $("#total-comment-out").text(result.data.length);
+});
